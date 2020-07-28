972bc77da15a772c6b4ec821bbe07ef27334b2df
TsukitoIwasaki <tiwasaki@toyo-group.co.jp>
2020-07-28 20:10:51 +0900

    1010

--- manage.py
+++ manage.py
@@ -1,12 +1,21 @@
 from __future__ import print_function
 
 from flask_script import Manager
+from flask_script import Server
 
 from flaskr import app
 from flaskr import db
 
 
 manager = Manager(app)
+
+manager.add_command(
+    "runserver", 
+    Server(
+    use_debugger = True,
+    use_reloader = True,
+    ) 
+)
 
 @manager.command
 def init_db():
