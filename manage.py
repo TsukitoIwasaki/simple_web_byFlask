528eecebda9d954c50937e70d19fa768c04a1758
TsukitoIwasaki <tiwasaki@toyo-group.co.jp>
2020-07-28 11:37:25 +0900

    OK

--- manage.py
+++ manage.py
@@ -1,2 +1,16 @@
+from __future__ import print_function
+
+from flask_script import Manager
+
 from flaskr import app
-app.run(host='127.0.0.1', port=5000, debug=True)
+from flaskr import db
+
+
+manager = Manager(app)
+
+@manager.command
+def init_db():
+    db.create_all()
+
+if __name__ == '__main__':
+    manager.run()
